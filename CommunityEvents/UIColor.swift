c5be5919af163029307c6f999592134327dad5af
[Jasmine Patel] <[jasmine.patel@footasylum.com]>
2022-06-09 22:50:04 +0100

    added new custom colours

---
+++ CommunityEvents/UIColor.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor.swift
+//  CommunityEvents
+//
+//  Created by Jasmine Patel on 09/06/2022.
+//
+
+import SwiftUI
+
+extension UIColor {
+    static var ceGreen: UIColor { UIColor(named: "ceGreen")! }
+    static var ceOrange: UIColor { UIColor(named: "ceOrange")! }
+    static var ceGrey: UIColor { UIColor(named: "ceGrey")! }
+}
+
+extension Color {
+    static var ceGreen: Color { Color(.ceGreen) }
+    static var ceOrange: Color { Color(.ceOrange) }
+    static var ceGrey: Color { Color(.ceGrey) }
+}
